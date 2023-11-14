ecd52e2252e815206dee27b084bc0c3ffa64ef40
Louis Whisenant <louiswhisenant@gmail.com>
2023-11-14 16:58:14 -0600

    non-admin user routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import cookieParser from 'cookie-parser';
+
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -11,11 +15,17 @@ connectDB();
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
 	res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
